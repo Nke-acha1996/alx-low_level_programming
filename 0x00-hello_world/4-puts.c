b35129524718c5cc93b967a077c85d7ecee44a6d
Nke-acha1996 <chukwudinkechinyere2016@gmail>
2022-12-10 10:03:46 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,22 @@
-puts
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for(i = 97; i< 123;i++)
+	{
+
+		putchar(i)
+
+	}
+}
+putchar('\n')
+return (0)
+	}
